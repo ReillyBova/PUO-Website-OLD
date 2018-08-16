d5d856dedc9e79ea323b71554e57074db5c7a521
Reilly Bova <rbova@princeton.edu>
2018-08-16 05:41:56 -0400

    Added new video sizes and improved sizing logic for video

--- src/assets/videos/index.js
+++ src/assets/videos/index.js
@@ -1,1 +1,5 @@
-export { default as previewVideo } from './output.mp4'
+export { default as largePreviewMP4 } from './large.mp4'
+export { default as largePreviewWebM } from './large.webm'
+export { default as mediumPreviewMP4 } from './medium.mp4'
+export { default as smallPreviewMP4 } from './small.mp4'
+export { default as tinyPreviewMP4 } from './tiny.mp4'
--- src/pages/home-page/homePage.jsx
+++ src/pages/home-page/homePage.jsx
@@ -1,30 +1,74 @@
 import React from 'react';
 import styled from 'styled-components'
-import { previewVideo } from 'assets/videos'
+import { largePreviewMP4 } from 'assets/videos'
+import $ from 'jquery'
 
 const VideoWrapper = styled.div`
-padding-top: 50px;
-width: 100%;
-max-height: calc(100vh - 70px);
-overflow: hidden;
+  padding-top: 40px;
+  width: 100%;
+  height: calc(100vh - 110px);
+  overflow: hidden;
+`
+
+const Video = styled.video`
+  &.wide {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+  }
+
+  &:not(.wide) {
+    margin-left: 50%;
+    transform: translateX(-50%);
+    height: 100%;
+  }
 `
 
 export class HomePage extends React.Component {
-  state = {
-    loaded: false
+  constructor(props) {
+    super(props);
+    this.resize_video = this.resize_video.bind(this);
+
+    this.state = {
+      loaded: false
+    }
+  }
+
+  isScreenTall() {
+    const wrapper = $('#previewWrapper')
+    const ratio = wrapper.width() / wrapper.height()
+
+    return (16.0 / 9.0 - ratio > 0)
+  }
+
+  resize_video() {
+    if (this.isScreenTall()) $('#previewVideo').removeClass('wide')
+    else $('#previewVideo').addClass('wide')
   }
 
   componentDidMount() {
     this.setState({loaded: true})
+
+    $(window).resize(this.resize_video);
+    this.resize_video()
   }
 
   render() {
     return (
-      <VideoWrapper>
+      <VideoWrapper id="previewWrapper">
         { this.state.loaded &&
-        <video style={{width: '100%', height: '100%'}} playsInline muted loop autoPlay preload="none">
-          <source type="video/mp4" src={previewVideo} />
-        </video>}
+        <Video
+          playsInline
+          muted
+          loop
+          autoPlay
+          preload="auto"
+          className={`${this.isScreenTall() ? null : 'wide'}`}
+          id="previewVideo"
+          style={{}}
+        >
+          <source type="video/mp4" src={largePreviewMP4} />
+        </Video>}
       </VideoWrapper>
     )
   }
