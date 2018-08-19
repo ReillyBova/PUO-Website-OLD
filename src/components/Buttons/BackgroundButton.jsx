b6be592b372668f85591933f7251941245a26140
Reilly Bova <rbova@princeton.edu>
2018-08-19 17:35:47 -0400

    Fixed button styles

--- src/components/Buttons/BackgroundButton.jsx
+++ src/components/Buttons/BackgroundButton.jsx
@@ -1,47 +1,41 @@
 import React from 'react';
 import styled from 'styled-components'
 import Button from '@material-ui/core/Button';
+import ArrowRightAlt from '@material-ui/icons/ArrowRightAlt';
+import { Link } from 'react-router-dom'
 
 const StyledButton = styled(Button)`
-  &.MuiButton-textPrimary-4 {
-    color: ${props => props.theme.black} !important;
-    background-color: rgba(0, 0, 0, 0.2);
-    &:hover {
-      background-color: ${props => props.theme.orange};
-    }
-  }
-  &.MuiButton-root-1 {
-    font-size: 18px;
-    border-radius: 2px;
-  }
-  &.MuiButton-outlinedPrimary-10 {
-    border: 2px solid white !important;
-    &:hover {
-      border: 2px solid ${props => props.theme.orange} !important;
-    }
+  font-size: 18px !important;
+  border-radius: 2px !important;
+  border: 2px solid white !important;
+  color: ${props => props.theme.black} !important;
+  background-color: rgba(0, 0, 0, 0.2) !important;
+  &:hover {
+    background-color: ${props => props.theme.orange} !important;
+    border: 2px solid ${props => props.theme.orange} !important;
   }
 
-  .MuiButton-label-2 {
+  &>span:first-of-type {
     color: white !important;
     position: relative;
     z-index: 1;
   }
 
-  .MuiTouchRipple-root-30 {
-    border-radius: 0;
-  }
+  &>span:last-of-type  {
+    border-radius: 0 !important;
 
-  .MuiTouchRipple-rippleVisible-32 {
-    opacity: 1 !important;
+    span {
+      opacity: 1 !important;
 
-    @keyframes mui-ripple-enter {
-      0% {
-        opacity: 0.75;
-        transform: scale(0);
-      }
-      100% {
-        opacity: 1;
-        transform: scale(1);
+      @keyframes mui-ripple-enter {
+        0% {
+          opacity: 0.75;
+          transform: scale(0);
+        }
+        100% {
+          opacity: 1;
+          transform: scale(1);
+        }
       }
     }
   }
@@ -49,9 +43,12 @@ const StyledButton = styled(Button)`
 
 const BackgroundButton = ({children}) => {
   return (
-    <StyledButton variant="outlined" color="primary"  >
-      { children }
-    </StyledButton>
+    <Link to={'${process.env.PUBLIC_URL}/concerts'}>
+      <StyledButton variant="outlined" color="primary"  >
+        { children }
+        <ArrowRightAlt style={{marginLeft: 8}} />
+      </StyledButton>
+    </Link>
   )
 }
 
